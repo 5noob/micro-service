3a38927e0376a6e4b7975bf770ed66e5595930a0
欧阳杰 <ouyangjie@tuandai.com>
2019-02-17 18:15:44 +0800

    feat: 进一步验证AutoConfigurationImportSelector的作用

---
+++ autoconfig-test/src/main/java/com/me/springaplication/SamePackageExample.java
@@ -0,0 +1,12 @@
+package com.me.springaplication;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author OuyangJie
+ * @Date 2019/2/17 17:58
+ * @Description:
+ */
+@Component
+public class SamePackageExample {
+}
--- spring-application/src/main/java/com.me.springaplication/MicroServiceApplication.java
+++ spring-application/src/main/java/com.me.springaplication/MicroServiceApplication.java
@@ -24,14 +24,21 @@ public class MicroServiceApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(MicroServiceApplication.class, args);
 
-        //验证@EnableAutoConfiguration Import的AutoConfigurationImportSelector.class的作用
+        //1、验证@EnableAutoConfiguration Import的AutoConfigurationImportSelector.class的作用
+        /**
+         * 需要在spring.factories文件中加入org.springframework.boot.autoconfigure.EnableAutoConfiguration=@Configuration标记的类的路径
+         */
         Cat cat = context.getBean(Cat.class);
         System.out.println(cat);
+        //不需要在spring.factories文件加路径，因为SamePackageExample的包路径和启动类的包路径相同
+        SamePackageExample samePackageExample = context.getBean(SamePackageExample.class);
+        System.out.println(samePackageExample);
 
-        //验证@EnableAutoConfiguration Import的AutoConfigurationPackages.Registrar.class的作用
+        //2、验证@EnableAutoConfiguration Import的AutoConfigurationPackages.Registrar.class的作用
         Object autoConfigurationPackages = context.getBean(AutoConfigurationPackages.class.getName());
         System.out.println(autoConfigurationPackages);
 
+        //3、验证启动类，是否注入了spring的bean池中
         MicroServiceApplication microServiceApplication = context.getBean(MicroServiceApplication.class);
         System.out.println(microServiceApplication);
     }
